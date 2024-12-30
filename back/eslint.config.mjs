4750775d110da6d1e244131496fd7c1bbc678a5c
tota-kn <nonbiriactive@gmail.com>
2024-12-30 22:29:54 +0900

    update

---
+++ back/eslint.config.mjs
@@ -0,0 +1,7 @@
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default tseslint.config(
+
+)
