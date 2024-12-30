9c5eface08a57520ff51f9b97052f12592892460
tota-kn <nonbiriactive@gmail.com>
2024-12-30 23:10:30 +0900

    update

--- back/src/handlers/test/get.ts
+++ back/src/handlers/test/get.ts
@@ -7,7 +7,7 @@ export const handler: Handler = async (
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from the back!!",
+      message: "Hello from the back!!!",
     }),
   };
 };
