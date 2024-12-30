5c7c821409e29b8611309a9bd631f26ca907d010
tota-kn <nonbiriactive@gmail.com>
2024-12-30 22:49:30 +0900

    update

--- back/eslint.config.mjs
+++ back/eslint.config.mjs
@@ -1,7 +1,40 @@
+import { includeIgnoreFile } from "@eslint/compat";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, '.gitignore');
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default tseslint.config(
+  /** 推奨設定 */
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
 
-)
+  /** 対象ファイルの設定 */
+  {
+    files: ["**/*.{js,ts,mjs,mts,cjs,cts,jsx,tsx}"],
+  },
+  includeIgnoreFile(gitignorePath),
+
+  /** 構文解析設定 */
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // consoleの定義
+        ...globals.node, // processの定義
+      },
+      parserOptions: {
+        ecmaVersion: "ES2024",
+        sourceType: "module",
+      },
+    },
+  },
+
+  // Prettier併用設定
+  eslintConfigPrettier,
+);
--- back/src/handlers/test/get.ts
+++ back/src/handlers/test/get.ts
@@ -1,11 +1,13 @@
-import { APIGatewayEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
+import { APIGatewayEvent, APIGatewayProxyResult, Handler } from "aws-lambda";
 
-export const handler: Handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-    console.log('Hello from the back!!!');
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Hello from the back!!'
-        }),
-    };
-}
+export const handler: Handler = async (
+  event: APIGatewayEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log("Hello from the back!!!", JSON.stringify(event));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello from the back!!",
+    }),
+  };
+};
