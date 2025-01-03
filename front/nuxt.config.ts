fea5c517cce6b03ffcc5e52847ce36bfd68fc649
tota-kn <nonbiriactive@gmail.com>
2025-01-03 23:28:50 +0900

    update

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,25 +1,25 @@
-import { defineNuxtConfig } from 'nuxt/config';
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  modules: ['nitro-cloudflare-dev',
-    '@nuxt/eslint'],
+  modules: ["nitro-cloudflare-dev",
+    "@nuxt/eslint"],
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      API_BASE_URL: '',
-      TITLE: 'Web Template Front',
+      API_BASE_URL: "",
+      TITLE: "Web Template Front",
     },
   },
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   nitro: {
-    preset: 'cloudflare-pages',
+    preset: "cloudflare-pages",
   },
   eslint: {
     config: {
       stylistic: {
         indent: 2,
         semi: true,
-        quotes: 'double',
+        quotes: "double",
       },
     },
   },
