dfd31b8472b253f49d3f3e0e1b26b0d15206e43d
tota-kn <nonbiriactive@gmail.com>
2025-01-04 18:23:41 +0900

    update

--- front/stories/StoreCountButton.stories.ts
+++ front/stories/StoreCountButton.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 import StoreCountButton from "~/components/StoreCountButton.vue";
 
 const meta = {
-  title: "Components",
+  title: "StoreCountButton",
   component: StoreCountButton,
   tags: ["autodocs"],
 
