0acdce1c7169bcd17c62f4e17e927cca0060a2a0
tota-kn <nonbiriactive@gmail.com>
2025-01-05 17:01:26 +0900

    update

--- back/dev.sh
+++ back/dev.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-tmux split-window -h
-tmux send-keys "pnpm run dev:esbuild" C-m
+DIR=$(dirname "$0")
 
 tmux split-window -h
-tmux send-keys "pnpm run dev:tsc" C-m
+tmux send-keys "cd ${DIR}/../front && pnpm run dev" C-m
 
 tmux split-window -h
-tmux send-keys "pnpm run dev:sam" C-m
+tmux send-keys "cd ${DIR} && pnpm run dev:esbuild" C-m
 
 tmux split-window -h
-tmux send-keys "pnpm run dev:test" C-m
+tmux send-keys "cd ${DIR} && pnpm run dev:tsc" C-m
+
+tmux split-window -h
+tmux send-keys "cd ${DIR} && pnpm run dev:sam" C-m
+
+tmux split-window -h
+tmux send-keys "cd ${DIR} && pnpm run dev:test" C-m
 
 tmux select-layout tiled
